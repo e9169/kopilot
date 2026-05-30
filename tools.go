847c6ae5e03c84585021316f6e0c89215a40dda3
Eneko P <e9169@users.noreply.github.com>
2026-05-30 13:12:56 +0200

    fix: update comment to clarify gosec pinning in tools.go

--- tools.go
+++ tools.go
@@ -2,4 +2,4 @@
 
 package main
 
-import _ "github.com/securego/gosec/v2/cmd/gosec"
+import _ "github.com/securego/gosec/v2/cmd/gosec" // pin gosec in go.sum so the CI action uses a verified version
